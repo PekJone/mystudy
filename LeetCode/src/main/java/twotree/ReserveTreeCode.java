263f4d9b83ee96e8e60b80b4d815b3e3d53c5b37
wang <742662307@qq.com>
2022-06-15 22:30:47 +0800

    LeetCode 反转二叉树

---
+++ LeetCode/src/main/java/twotree/ReserveTreeCode.java
@@ -0,0 +1,34 @@
+package twotree;
+
+public class ReserveTreeCode {
+
+
+    /**
+     * 中序遍历
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree1(TreeNode root){
+        if(root==null){
+            return root;
+        }
+        invertTree1(root.left);
+        TreeNode rightNode = root.right;
+        root.right = root.left;
+        root.left = rightNode;
+        invertTree1(root.left);
+        return root;
+    }
+
+    public TreeNode invertTree2(TreeNode root){
+        if(root==null){
+            return root;
+        }
+        //保存右子树
+        TreeNode rightTree = root.right;
+        //交换左右子树的位置
+        root.right = invertTree2(root.left);
+        root.left = invertTree2(rightTree);
+        return root;
+    }
+}
